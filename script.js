9e287fc34ade36c5f308670dd32578534a96e3d5
Willfoss <118803774+Willfoss@users.noreply.github.com>
2023-11-15 21:05:53 +0000

    moved equals button in html and created four calculator functions in JS file

---
+++ script.js
@@ -0,0 +1,16 @@
+function addition(number1,number2){
+    return number1+number2;
+}
+
+function multiply(number1, number2) {
+    console.log(`final ${number1*number2}`);
+    return number1*number2;
+}
+
+function subtract(number1, number2) {
+    return number1-number2;
+}
+
+function divide(number1, number2) {
+    return number1/number2;
+}
